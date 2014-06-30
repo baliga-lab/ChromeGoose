0a8a55b388685bad26fd74a859c4419e43694c99
njiang <jiangbuf@gmail.com>
2014-06-30 16:06:28 -0700

    Add MTB tfoe filter function.

--- handlers/tfoefilter.js
+++ handlers/tfoefilter.js
@@ -1,10 +1,23 @@
 var gaggletfoefilter = {
     parseData: function(host, packagename, functionname, sessionid, species)
     {
-        var url = host + "/tmp/" + sessionid + "/R/.val";
-        console.log("opencpu url: " + url);
-        $.get(url, function(data) {
-            console.log("Received data: " + data);
-        }
+        var url = host + "/tmp/" + sessionid + "/graphics/1";
+        console.log("opencpu tfoe graphics url: " + url);
+        var html = "<div class='gaggle-plottfoe' style='display: none'><input type='text' value='" +url + "' /></div>";
+        console.log("html: " + html);
+        var wrapdiv = document.createElement("div");
+        wrapdiv.setAttribute("id", "divNewGaggledData");
+        $(wrapdiv).html(html);
+        document.body.appendChild(wrapdiv);
+
+        url = host + "/tmp/" + sessionid + "/stdout";
+        $.get(url, function(data){
+            console.log("TFOE script received data: " + data);
+
+            console.log("Send GaggleDataAddEvent event...");
+            var event = new CustomEvent('GaggleDataAddEvent', {detail: {funcname: functionname, species: species, handlername: "tfoefilter", output: data},
+                                         bubbles: true, cancelable: false});
+            document.dispatchEvent(event);
+        });
     }
-}
+};
