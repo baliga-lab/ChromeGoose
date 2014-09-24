74f0402cd4f3ce96be67d17aa32336d268514fee
Ning Jiang <jiangbuf@gmail.com>
2014-09-24 10:49:57 -0700

    new opencpu server IP

--- util.js
+++ util.js
@@ -17,7 +17,7 @@ var BOSS_JNLP = GAGGLE_SERVER + "/static/jnlp/boss.jnlp";
 var HTTPBOSS_ADDRESS = "ws://localhost:8083/BossWebSocket";
 var WEBHANDLER_BASEURL = GAGGLE_SERVER + "/static/javascripts/handlers/";
 //var OPENCPU_SERVER = "http://10.10.3.175/ocpu";
-var OPENCPU_SERVER = "http://54.191.211.80/ocpu";
+var OPENCPU_SERVER = "http://54.69.117.206/ocpu";
 var DEBUGGING = false;// Set this to false in production. It controls whether we log data on google analytics
 
 var cg_util = {
